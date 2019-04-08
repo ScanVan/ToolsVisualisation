775b3da333938e8943f32aeec422997d0acbe958
Marcelo Kaihara <marcelo.kaihara@gmail.com>
2019-04-08 17:09:33 +0200

    Added the visualization routines for triplets

--- README.md
+++ README.md
@@ -8,10 +8,14 @@ The usage of the program is the following:
 Where config_file.txt is a text file with the following parameters.
 
 ```
+# Lines starting with # will be ommitted.
 # Path to the folder with equirectnagular images
 PATH_TO_EQUI =path_to_output_folder_of_converted_equirectangular_images
 # Path to the folder with the models
 PATH_TO_MODEL =path_to_the_models
+# To show the matches between pair of images, use the parameter FILE_MATCHES2, otherwise, to show matches of triplets use the parameter FILE_MATCHES3
 # File name of the file with the pair of matches
-FILE_MATCHES2 = 20190319-103906-094891_20190319-103906-344895
+#FILE_MATCHES2 = 20190319-103906-094891_20190319-103906-344895
+# File name of the file with the triplets of matches
+FILE_MATCHES3 = 20190319-103949-594899_20190319-103949-844896_20190319-103950-094893
 ```
--- src/ToolsVisualisation-CPP.cpp
+++ src/ToolsVisualisation-CPP.cpp
@@ -26,16 +26,17 @@ public:
 	std::string ConfigFilePath {"./"};
 	std::string path_to_equi {"./"};
 	std::string path_to_model {"./"};
-	std::string file_2_matches {"./"};
+	std::string file_2_matches {""};
+	std::string file_3_matches {""};
 	PathClass(std::string s): ConfigFilePath(s) {};
 	PathClass(std::string path_to_equi, std::string path_to_model):
 		path_to_equi(path_to_equi), path_to_model(path_to_model) {}
-	PathClass(std::string path_to_equi, std::string path_to_model, std::string file_2_matches):
-			path_to_equi(path_to_equi), path_to_model(path_to_model), file_2_matches(file_2_matches) {}
 	void SetFile2Matches (std::string file) {
 		file_2_matches = file;
 	}
-
+	void SetFile3Matches (std::string file) {
+		file_3_matches = file;
+	}
 
 };
 
@@ -123,6 +124,13 @@ void ProcessConfigFile(PathClass &FC) {
 			} else {
 				throw(std::runtime_error("Error: FILE_MATCHES2 parameter is empty."));
 			}
+		} else if (command == "FILE_MATCHES3") {
+			// File name of the matches to analyze - triplets
+			if (argument != "") {
+				FC.file_3_matches = argument;
+			} else {
+				throw(std::runtime_error("Error: FILE_MATCHES3 parameter is empty."));
+			}
 		}
 	}
 }
@@ -171,6 +179,60 @@ void LoadMatches2 (PathClass &PC, std::vector<std::vector<double>> &P1, std::vec
 	}
 
 }
+
+void LoadMatches3 (PathClass &PC, std::vector<std::vector<double>> &P1, std::vector<std::vector<double>> &P2, std::vector<std::vector<double>> &P3) {
+
+	fs::path path2Models = PC.path_to_model;
+	fs::path path2Matches = ((path2Models / std::string("output")) / std::string("3_triplets")) / PC.file_3_matches;
+
+	std::ifstream f (path2Matches.string());
+
+	if (!f.good()) {
+		throw std::runtime_error("Could not open the file with the matches.");
+	}
+
+	P1.clear();
+	P2.clear();
+	P3.clear();
+
+	while (f.good()) {
+		std::string s {};
+		double x1{}, y1{}, x2{}, y2{}, x3{}, y3{};
+
+		std::getline(f, s);
+		if (s != "") {
+			std::stringstream ss { };
+
+			ss << s;
+
+			ss >> std::setprecision(15) >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+			std::vector<double> point1 {};
+			point1.push_back(x1);
+			point1.push_back(y1);
+
+			P1.push_back(point1);
+
+			std::vector<double> point2 {};
+			point2.push_back(x2);
+			point2.push_back(y2);
+
+			P2.push_back(point2);
+
+			std::vector<double> point3 { };
+			point3.push_back(x3);
+			point3.push_back(y3);
+
+			P3.push_back(point3);
+
+			//std::cout << std::setprecision(15) << x1 << " " << y1 << " " << x2 << " " << y2 << " " << x3 << " " << y3 << std::endl;
+
+		}
+
+	}
+
+}
+
 
 double CalcMeanDistance (std::vector<std::vector<double>> &P1, std::vector<std::vector<double>> &P2) {
 
@@ -283,8 +345,140 @@ void ShowMatches2 (PathClass &PC) {
 
 	cv::waitKey(0);
 
+}
+
+
+void ShowMatches3 (PathClass &PC) {
+
+	cv::RNG rng(12345);
+
+	// File name of the matches of triplets of images
+	std::string fileTriplet = PC.file_3_matches;
+
+	// Extract name of the 3 images separated by "_"
+	const auto posUnderscore = fileTriplet.find_first_of("_");
+	std::string file1 = fileTriplet.substr(0, posUnderscore) + ".bmp";
+	std::string file23 = fileTriplet.substr(posUnderscore + 1);
+	const auto posUnderscore2 = file23.find_first_of("_");
+	std::string file2 = file23.substr(0, posUnderscore2) + ".bmp";
+	std::string file3 = file23.substr(posUnderscore2 + 1) + ".bmp";
+
+
+	cv::Mat img1 {};
+	cv::Mat img2 {};
+	cv::Mat img3 {};
+
+	// Generate the full path to the image 1
+	fs::path pathEqui = PC.path_to_equi;
+	fs::path pathEqui1 = pathEqui / file1;
+	std::string path_to_equi_1 = pathEqui1.string();
+
+	img1 = cv::imread(path_to_equi_1);
+	if (!img1.data) {
+		throw std::runtime_error("Could not load the equirectangular image.");
+	}
+
+	// Generate the full path to the image 2
+	fs::path pathEqui2 = pathEqui / file2;
+	std::string path_to_equi_2 = pathEqui2.string();
+
+	img2 = cv::imread(path_to_equi_2);
+	if (!img2.data) {
+		throw std::runtime_error("Could not load the equirectangular image.");
+	}
+
+	// Generate the full path to the image 3
+	fs::path pathEqui3 = pathEqui / file3;
+	std::string path_to_equi_3 = pathEqui3.string();
+
+	img3 = cv::imread(path_to_equi_3);
+	if (!img3.data) {
+		throw std::runtime_error("Could not load the equirectangular image.");
+	}
+
+	std::vector<std::vector<double>> P1;
+	std::vector<std::vector<double>> P2;
+	std::vector<std::vector<double>> P3;
+
+
+	LoadMatches3(PC, P1, P2, P3);
+
+	//std::cout << "The mean distance of the features from image 1 and image 2 is: " << CalcMeanDistance(P1, P2) << std::endl;
+
+	cv::Mat bgr1[3] { };
+	cv::split(img1, bgr1);
+	cv::Mat bgr2[3] { };
+	cv::split(img2, bgr2);
+	cv::Mat bgr3[3] { };
+	cv::split(img3, bgr3);
+
+	std::vector<cv::Mat> images(3);
+
+	for (const auto &point1:P1) {
+		cv::circle (img1, cv::Point(point1[0],point1[1]), 5.0, cv::Scalar(0,255,0),1,8);
+	}
+
+
+	for (const auto &point2:P2) {
+		cv::circle (img2, cv::Point(point2[0],point2[1]), 5.0, cv::Scalar(0,255,0),1,8);
+	}
+
+	for (const auto &point3:P3) {
+		cv::circle (img3, cv::Point(point3[0],point3[1]), 5.0, cv::Scalar(0,255,0),1,8);
+	}
+
+	images.at(0) = bgr1[0];
+	images.at(1) = bgr2[1];
+	images.at(2) = bgr3[2];
+
+	cv::Mat img4 {};
+	cv::merge(images, img4);
+
+	for (const auto &point1 : P1) {
+		cv::circle(img4, cv::Point(point1[0], point1[1]), 5.0, cv::Scalar(0, 255, 255), 1, 8);
+	}
+
+	for (const auto &point2 : P2) {
+		cv::circle(img4, cv::Point(point2[0], point2[1]), 5.0, cv::Scalar(0, 255, 255), 1, 8);
+	}
+
+	for (const auto &point3 : P3) {
+		cv::circle(img4, cv::Point(point3[0], point3[1]), 5.0, cv::Scalar(0, 255, 255), 1, 8);
+	}
+
+	for (size_t i{0}; i < P1.size(); ++i) {
+		cv::Scalar lineColor (rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		cv::line(img4, cv::Point(P1[i][0], P1[i][1]), cv::Point(P2[i][0], P2[i][1]), lineColor, 2, cv::LINE_AA);
+		cv::line(img4, cv::Point(P2[i][0], P2[i][1]), cv::Point(P3[i][0], P3[i][1]), lineColor, 2, cv::LINE_AA);
+	}
+
+	cv::namedWindow("Image 4",  cv::WINDOW_NORMAL);
+	cv::imshow("Image 4", img4);
+
+
+	cv::namedWindow("Image 1",  cv::WINDOW_NORMAL);
+	cv::imshow("Image 1", img1);
+
+	cv::namedWindow("Image 2", cv::WINDOW_NORMAL);
+	cv::imshow("Image 2", img2);
+
+	cv::namedWindow("Image 3", cv::WINDOW_NORMAL);
+	cv::imshow("Image 3", img3);
+
+	cv::waitKey(0);
 
 }
+
+void ShowMatches (PathClass &PC) {
+
+	if (PC.file_2_matches!="") {
+		ShowMatches2 (PC);
+	} else {
+		ShowMatches3 (PC);
+	}
+
+}
+
 
 int main(int argc, char* argv[]) {
 
@@ -301,7 +495,7 @@ int main(int argc, char* argv[]) {
 	// Process the configuration file
 	ProcessConfigFile (PC);
 
-	ShowMatches2 (PC);
+	ShowMatches (PC);
 
 	return 0;
 }
